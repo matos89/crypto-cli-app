d1e775fd7904ffc7068535f27b423e3c59710eed
github username <github email address>
2017-10-21 18:41:47 -0700

    set up require & require_relatives

--- lib/crypto.rb
+++ lib/crypto.rb
@@ -1,3 +1,12 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require_relative './crypto/cli'
+require_relative './crypto/scraper'
+require_relative './crypto/coin'
+
+
+
 module Crypto
   VERSION = "0.1.0"
 end
